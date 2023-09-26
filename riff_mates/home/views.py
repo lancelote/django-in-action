855885d51242241e8729a46d38ebf7e7d9b66d1a
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-09-26 21:18:07 +0200

    Add `credits` view

--- riff_mates/home/views.py
+++ riff_mates/home/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def credits(request):
+    content = "Nicky\nYour Name"
+    return HttpResponse(content, content_type="text/plain")
