5ea888936a365ce59e7f1bd5c14bd79f1db66a13
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-10-31 14:46:56 +0100

    Add musicians view

--- riff_mates/bands/urls.py
+++ riff_mates/bands/urls.py
@@ -4,4 +4,5 @@ from bands import views
 
 urlpatterns = [
     path('musician/<int:musician_id>/', views.musician, name='musician'),
+    path('musicians/', views.musicians, name='musicians'),
 ]
--- riff_mates/bands/views.py
+++ riff_mates/bands/views.py
@@ -7,3 +7,8 @@ from bands.models import Musician
 def musician(request, musician_id):
     data = {"musician": get_object_or_404(Musician, id=musician_id)}
     return render(request, "musician.html", data)
+
+
+def musicians(request):
+    data = {"musicians": Musician.objects.all().order_by("last_name")}
+    return render(request, "musicians.html", data)
