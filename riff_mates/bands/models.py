131848ec83de7ca899d9351928febc26e99742b0
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-11-01 16:51:50 +0100

    Add Venue, Room, and Band models

--- riff_mates/bands/models.py
+++ riff_mates/bands/models.py
@@ -8,3 +8,26 @@ class Musician(models.Model):
 
     def __str__(self) -> str:
         return f"Musician(id={self.id}, last_name={self.last_name})"
+
+
+class Venue(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return f"Venue(if={self.id}, name={self.name})"
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=20)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"Room(id={self.id}, name={self.name})"
+
+
+class Band(models.Model):
+    name = models.CharField(max_length=20)
+    musicians = models.ManyToManyField(Musician)
+
+    def __str__(self) -> str:
+        return f"Band(id={self.id}, name={self.name})"
