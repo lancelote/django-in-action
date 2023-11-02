27683643a6aa30fc38c6a5a6283957fdfabb5611
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-11-02 20:51:53 +0100

    Register Musician in admin

--- riff_mates/bands/admin.py
+++ riff_mates/bands/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from bands.models import Musician
+
+
+@admin.register(Musician)
+class MusicianAdmin(admin.ModelAdmin):
+    list_display = ("id", "last_name", "show_weekday")
+
+    def show_weekday(self, obj):
+        return obj.birth.strftime("%A")
+
+    show_weekday.short_description = "Birth Weekday"
