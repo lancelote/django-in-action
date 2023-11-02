5df7bb7bab38ab617dfe3a78f6406dd06688f4ec
Pavel Karateev <lancelote.du.lac@gmail.com>
2023-11-02 12:00:26 +0100

    Venues view

--- riff_mates/bands/urls.py
+++ riff_mates/bands/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('bands/', views.bands, name='bands'),
     path('musician/<int:musician_id>/', views.musician, name='musician'),
     path('musicians/', views.musicians, name='musicians'),
+    path('venue/<int:venue_id>/', views.venue, name='venue'),
+    path('venues/', views.venues, name='venues'),
+    path('room/<int:room_id>', views.room, name='room'),
 ]
--- riff_mates/bands/views.py
+++ riff_mates/bands/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import get_object_or_404
 
 from bands.models import Band
 from bands.models import Musician
+from bands.models import Room
+from bands.models import Venue
 
 DEFAULT_PER_PAGE = 2
 MAXIMUM_PER_PAGE = 5
@@ -17,6 +19,16 @@ def musician(request, musician_id):
 def band(request, band_id):
     data = {"band": get_object_or_404(Band, id=band_id)}
     return render(request, "band.html", data)
+
+
+def venue(request, venue_id):
+    data = {"venue": get_object_or_404(Venue, id=venue_id)}
+    return render(request, "venue.html", data)
+
+
+def room(request, room_id):
+    data = {"room": get_object_or_404(Room, id=room_id)}
+    return render(request, "room.html", data)
 
 
 def get_items_per_page(request):
@@ -73,6 +85,25 @@ def bands(request):
         "bands.html",
         {
             "bands": page.object_list,
+            "page": page,
+            "per_page": per_page,
+        }
+    )
+
+
+def venues(request):
+    all_venues = Venue.objects.all().order_by("name")
+
+    per_page = get_items_per_page(request)
+    paginator = Paginator(all_venues, per_page)
+    page_num = get_page_num(request, paginator)
+    page = paginator.page(page_num)
+
+    return render(
+        request,
+        "venues.html",
+        {
+            "venues": page.object_list,
             "page": page,
             "per_page": per_page,
         }
